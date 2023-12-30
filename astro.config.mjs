56a9535c9c4e8d3f6ff8a21c004d5d1494d11a99
Gengchen <tuogengchen@gmail.com>
2023-12-29 17:38:43 -0700

    improve metadatas

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,52 +2,52 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
 
-import rehypeKatex from 'rehype-katex'
-import remarkMath from 'remark-math'
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 import rehypePrettyCode from 'rehype-pretty-code';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
-import rehypeSlug from 'rehype-slug'
+import rehypeSlug from 'rehype-slug';
 import remarkGfm from 'remark-gfm';
 
-import TokyoNight from './src/TokyoNight.json'
-import TokyoNightLight from './src/TokyoNightLight.json'
+import TokyoNight from './src/TokyoNight.json';
+import TokyoNightLight from './src/TokyoNightLight.json';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-blog-weld-ten.vercel.app',
+  site: 'https://tgc54.com',
   prefetch: {
     defaultStrategy: 'viewport'
   },
   image: {
-    domains: ["astro.build", "codeforces.com", "user-images.githubusercontent.com", "cdn.sparkfun.com"],
+    domains: ["astro.build", "codeforces.com", "user-images.githubusercontent.com", "cdn.sparkfun.com"]
   },
   markdown: {
     syntaxHighlight: false,
     remarkPlugins: [remarkMath, remarkGfm],
-    rehypePlugins: [
-      [rehypeKatex, { strict: true, throwOnError: true }],
-      [rehypePrettyCode, {
-        theme: {
-          dark: TokyoNight,
-          light: TokyoNightLight
-        }
-      }],
-      rehypeSlug,
-      [rehypeAutolinkHeadings, {
-        behavior: 'append',
-        content: { type: 'text', value: ' #' },
-        headingProperties: {
-          className: "group",
-        },
-        properties: {
-          ariaHidden: true,
-          tabIndex: -1,
-          className: "opacity-0 no-underline text-muted-foreground group-hover:opacity-100"
-        }
-      }],
-    ]
+    rehypePlugins: [[rehypeKatex, {
+      strict: true,
+      throwOnError: true
+    }], [rehypePrettyCode, {
+      theme: {
+        dark: TokyoNight,
+        light: TokyoNightLight
+      }
+    }], rehypeSlug, [rehypeAutolinkHeadings, {
+      behavior: 'append',
+      content: {
+        type: 'text',
+        value: ' #'
+      },
+      headingProperties: {
+        className: "group"
+      },
+      properties: {
+        ariaHidden: true,
+        tabIndex: -1,
+        className: "opacity-0 no-underline text-muted-foreground group-hover:opacity-100"
+      }
+    }]]
   },
   integrations: [mdx(), sitemap(), tailwind({
     applyBaseStyles: false
